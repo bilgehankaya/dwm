5f0aa50110a987eb267b1566085ef1e6e40d8075
Bilgehan Kaya <bilbat35@gmail.com>
2021-01-08 00:05:01 +0300

    auto commit

--- config.h
+++ config.h
@@ -70,6 +70,7 @@ static const Rule rules[] = {
 	{ "st-256color",    NULL,     NULL,           	    0,            0,           1,         0,        -1 },
 	{ "St",       NULL,     NULL,           	    0,            0,           1,         0,        -1 },
 	{ "Alacritty",NULL,     NULL,           	    0,            0,           1,         0,        -1 },
+	{ "kitty",    NULL,     NULL,           	    0,            0,           1,         0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 	    0,            0,           0,         1,        -1 }, /* xev */
 	{ NULL,      "spterm",    NULL,       	    SPTAG(0),     1,           1,         0,        -1 },
 	{ NULL,      "spcalc",    NULL,       	    SPTAG(1),     1,           1,         0,        -1 },
