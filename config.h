3341d2912950fdbf5fc7c4b56318d457baf2a885
Bilgehan Kaya <bilbat35@gmail.com>
2021-02-01 18:05:01 +0300

    auto commit

--- config.h
+++ config.h
@@ -216,7 +216,8 @@ static Key keys[] = {
 	{ MODKEY,			XK_F2,		spawn,		SHCMD("reloaddwm") },
 	{ MODKEY,			XK_F3,		spawn,		SHCMD("dmenudisplay") },
 	{ MODKEY,			XK_F4,		spawn,		SHCMD("dmenuscripts") },
-	{ MODKEY,			XK_F5,		spawn,		SHCMD("keyboard -t") },
+	{ MODKEY,			XK_F5,		spawn,		SHCMD("bootstrap") },
+    /* { MODKEY,			XK_F5,		spawn,		SHCMD("keyboard -t") }, */
 	{ MODKEY,			XK_F6,		spawn,		SHCMD("torwrap") },
 	{ MODKEY,			XK_F7,		spawn,		SHCMD("td-toggle") },
 	{ MODKEY,			XK_F8,		spawn,		SHCMD("dmenumount") },
